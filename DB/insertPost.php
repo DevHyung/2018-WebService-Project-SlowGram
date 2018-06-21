dd6212cb302d7a165fe4e307837afb08af29518f
DevHyung <khuphj@gmail.com>
2018-06-21 14:05:15 +0900

    Add insertPost PHP File

---
+++ DB/insertPost.php
@@ -0,0 +1,21 @@
+﻿<?php
+if(!isset($_SESSION)) 
+{ 
+    session_start(); 
+} 
+$name = $_SESSION['name'];
+$content=$_POST['content'];
+require_once("dbconfig.php");
+$sql = "INSERT INTO post (id,content,date,isPost) VALUES ('$name','$content',now(),'n');";
+if ($db->query($sql) === TRUE) {
+	mysqli_close($db);
+	echo"<script>alert('발송되었습니다.');history.go(-1);</script>";
+    
+} 
+//require "dbconfig.php";
+//$sql_new = "SELECT * from userlist where id = BINARY('$username')";
+//print_r($sql_new);
+//history.go(-1);
+
+
+?>
